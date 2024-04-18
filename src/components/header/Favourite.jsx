42fc8b095160818e96ee6c32734af532fbeb9c4b
Hanif <hanif@Hanifs-MacBook-Air.local>
2024-04-18 10:33:42 +0600

    Weather Board UI added

--- src/components/header/Favourite.jsx
+++ src/components/header/Favourite.jsx
@@ -1,7 +1,8 @@
+import favIcon from "./../../assets/heart.svg";
 export default function Favourite() {
   return (
     <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
-      <img src="./assets/heart.svg" alt="" />
+      <img src={favIcon} alt="" />
       <span>Favourite Locations</span>
     </div>
   );
