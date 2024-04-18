e4915484e83ac0f9faa7d00441a1b31021fbea75
Hanif <hanif@Hanifs-MacBook-Air.local>
2024-04-18 11:25:30 +0600

    design issue refactore

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import WeatherBoard from "./components/weather/WeatherBoard";
 
 export default function App() {
   return (
-    <>
+    <div className="grid place-items-center h-screen">
       <Header />
       <main>
         <section>
           <WeatherBoard />
         </section>
       </main>
-    </>
+    </div>
   );
 }
