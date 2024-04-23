e52d0797463c9553000aba77a0451c8f8f536bba
Hanif <hanif@Hanifs-MacBook-Air.local>
2024-04-23 22:47:18 +0600

    connected with api and fixing ui

--- src/components/weather/WeatherHeadline.jsx
+++ src/components/weather/WeatherHeadline.jsx
@@ -1,22 +1,44 @@
-import cloudIcon from "../../assets/cloud.svg";
+import { useContext } from "react";
+import CloudIcon from "../../assets/cloud.svg";
+import HazeIcon from "../../assets/haze.svg";
+import SnowIcon from "../../assets/icons/snow.svg";
+import SunnyIcon from "../../assets/icons/sunny.svg";
+import RainyIcon from "../../assets/rainy.svg";
+import ThunderIcon from "../../assets/thunder.svg";
+
 import pinIcon from "../../assets/pin.svg";
+import { WeatherContext } from "../../context";
+import { getFormattedDate } from "../../utils/date-util";
 
 export default function WeatherHeadline() {
+  const { weatherData } = useContext(WeatherContext);
+
+  const { climate, location, temperature, time, feelsLike } = weatherData;
+
+  console.log(CloudIcon, HazeIcon, RainyIcon, SunnyIcon, ThunderIcon, SnowIcon);
+
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
-        <img src={cloudIcon} alt="cloud" />
+        <img src={CloudIcon} alt={climate} />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-            16°
+            {Math.round(temperature)}°
           </h1>
+
+          <p className="text-sm lg:text-lg space-x-4 md:mb-4">
+            Feels Like: {Math.round(feelsLike)}°
+          </p>
           <div className="flex items-center space-x-4 md:mb-4">
             <img src={pinIcon} alt="pin" />
-            <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+            <h2 className="text-2xl lg:text-[50px]">{location}</h2>
           </div>
         </div>
       </div>
-      <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
+      <p className="text-sm lg:text-lg">
+        {getFormattedDate(time, "time", false)} -{" "}
+        {getFormattedDate(time, "date", false)}
+      </p>
     </div>
   );
 }
---
+++ src/hooks/useWeather.js
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+
+const useWeather = () => {
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    feelsLike: "",
+    maxTemperature: "",
+    minTemperature: "",
+    humidity: "",
+    cloudPercentage: "",
+    wind: "",
+    time: "",
+    longitude: "",
+    latitude: "",
+    sunrise: "",
+    sunset: "",
+  });
+  const [loading, setLoading] = useState({
+    state: false,
+    message: "",
+  });
+  const [error, setError] = useState(null);
+
+  const fetchWeatherData = async (latitude, longitude) => {
+    try {
+      setLoading({
+        ...loading,
+
+        state: true,
+        message: "Weather data fetching",
+      });
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${
+          import.meta.env.VITE_WEATHER_API_KEY
+        }&units=metric`
+      );
+      if (!response.ok) {
+        const errorMessage = `Fetching weather data failed: ${response.status}`;
+        throw new Error(errorMessage);
+      }
+      const data = await response.json();
+
+      const updateWeatherData = {
+        ...weatherData,
+        location: data?.name,
+        climate: data?.weather[0]?.main,
+        temperature: data?.main?.temp,
+        feelsLike: data?.main?.feels_like,
+        maxTemperature: data?.main?.temp_max,
+        minTemperature: data?.main?.temp_min,
+        humidity: data?.main?.humidity,
+        cloudPercentage: data?.clouds?.all,
+        wind: data?.wind?.speed,
+        time: data?.dt,
+        longitude,
+        latitude,
+        sunrise: data?.sys?.sunrise,
+        sunset: data?.sys?.sunset,
+      };
+      setWeatherData(updateWeatherData);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading({
+        ...loading,
+
+        state: false,
+        message: "",
+      });
+    }
+  };
+  useEffect(() => {
+    setLoading({
+      ...loading,
+      state: true,
+      message: "Finding Location...",
+    });
+    navigator.geolocation.getCurrentPosition(function (position) {
+      fetchWeatherData(position.coords.latitude, position.coords.longitude);
+    });
+
+    if (
+      !navigator.geolocation.getCurrentPosition(function (position) {
+        fetchWeatherData(position.coords.latitude, position.coords.longitude);
+      })
+    ) {
+      fetchWeatherData(23.750209710182087, 90.34542589506421);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    weatherData,
+    error,
+    loading,
+  };
+};
+export default useWeather;
